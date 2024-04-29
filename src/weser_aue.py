098b29896ed0a699effa7e106221a2d31fd1da64
AlexandraKapp <alexandra.kapp2@gmail.com>
2024-04-29 16:41:50 +0200

    correct bounding box

--- src/weser_aue.py
+++ src/weser_aue.py
@@ -4,17 +4,17 @@ import utils
 data_path = "data"
 ### USER INPUT: define bounding box ###
 
-minLon=9.154184848179721
-minLat=52.59201643401497
-maxLon=9.324129598910105
-maxLat=52.693989591636914
+minLon=8.922732223970124
+minLat=52.55664084766004
+maxLon=9.200080702809483
+maxLat=52.751913856234566
 name = "weser_aue"
 pred_path = "/Users/alexandra/Nextcloud-HTW/SHARED/SurfaceAI/data/mapillary_images/training/prediction/weseraue/effnet_surface_quality_prediction-weseraue_imgs_2048-20240425_165002.csv"
 
 
 data_path = os.path.join(data_path, name)
 
-#pl.img_sample(data_path, minLon, minLat, maxLon, maxLat, name, userid="109527895102408", no_pano=False) # weseraue2022
+pl.img_sample(data_path, minLon, minLat, maxLon, maxLat, name, userid="109527895102408", no_pano=False) # weseraue2022
 #pl.img_selection(data_path, minLon, minLat, maxLon, maxLat, name)
 #pl.img_download(name)
 #pl.img_download(data_path=data_path, db_table="weser_aue_point_selection")
@@ -28,4 +28,4 @@ data_path = os.path.join(data_path, name)
 #     utils.pano_to_perspective(img_id, os.path.join(data_path, "panorama_2048"), os.path.join(data_path, "imgs_2048"), angle=0.0)
     
 #pl.img_classification(data_path, name, pred_path)
-pl.aggregate_by_road_segment(name)
+#pl.aggregate_by_road_segment(name)
